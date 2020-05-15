aaa5b85711bd5a4ff8dc9d194a38a0e769e61630
AnnKemp <ann.kemp@scarlet.be>
2020-05-15 17:01:31 +0200

    Updates

---
+++ precache-manifest.e31dec8c15b7a685d12e70925cb47446.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e715eedc00d39a7d0b70de0a59f2b750",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "7f089cdb39e6f159c510",
+    "url": "/todolist/static/css/main.1dfee4b5.chunk.css"
+  },
+  {
+    "revision": "f4346897980c047e4898",
+    "url": "/todolist/static/js/2.6196e04d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.6196e04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f089cdb39e6f159c510",
+    "url": "/todolist/static/js/main.abff8813.chunk.js"
+  },
+  {
+    "revision": "658823e3cb77b75eedbc",
+    "url": "/todolist/static/js/runtime-main.587ab855.js"
+  }
+]);
